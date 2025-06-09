a0740fa51465360eb4d9aad44fd082aeb6a50237
Hefni R R A <hefniraera17@gmail.com>
2025-06-09 17:34:09 +0700

    Update database ENGINE to postgis for GIS support

--- smallfarming_project/settings.py
+++ smallfarming_project/settings.py
@@ -87,7 +87,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
